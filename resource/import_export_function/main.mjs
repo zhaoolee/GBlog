99ac71cc5738920ef7307af519d374aecdd39495
zhaoolee <zhaoolee@gmail.com>
2019-07-18 09:22:02 +0800

    以import from与export default结合方式同时导出多个变量

---
+++ resource/import_export_function/main.mjs
@@ -0,0 +1,7 @@
+import function_zhaoolee_info from './zhaoolee_info.mjs';
+let {name, age} = function_zhaoolee_info();
+
+
+
+// 运行方式为: node --experimental-modules main.mjs
+console.log(name,"的年龄为", age);
---
+++ resource/import_export_function/zhaoolee_info.mjs
@@ -0,0 +1,5 @@
+export default ()=>{
+  let name="zhaoolee";
+  let age = new Date().getFullYear() - 1995;
+  return {name, age};
+}

b4098d60221b5417ce328f832d0423b9b8abdead
zhaoolee <zhaoolee@gmail.com>
2019-12-09 17:35:14 +0800

    添加

---
+++ resource/node_env/index.js
@@ -0,0 +1,4 @@
+// 运行方式  NODE_ENV=production  node index.js
+console.log('当前环境变量NODE_ENV为=>'+process.env.NODE_ENV);
+// 运行方式  ZHAO_ENV=zhaoolee  node index.js
+console.log('当前环境变量ZHAO_ENV为=>'+process.env.ZHAO_ENV);
